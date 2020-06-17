11b0c1f5bc079b205fcb55e59909d82de89e370b
Hugues BONI <ing.huguesboni@gmail.com>
2020-06-17 10:44:20 +0100

    Product & Category migration created

---
+++ database/migrations/2020_06_17_091905_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sku')->unique();
+            $table->string('title', 500);
+            $table->text('description');
+            $table->float('price');
+            $table->float('stock_quantity');
+            $table->enum('quantity_unit', ['Kg', 'Tonne']);
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
